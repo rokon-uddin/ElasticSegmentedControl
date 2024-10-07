3c158d608c6011344f64135cbab6f2225e0fa19b
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-10-07 11:32:11 +0600

    Initial Commit

---
+++ ElasticSegmentedControl/ElasticSegmentedControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElasticSegmentedControlApp.swift
+//  ElasticSegmentedControl
+//
+//  Created by Mohammed Rokon Uddin on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ElasticSegmentedControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
